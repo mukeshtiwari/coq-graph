3970fcb3946dbf3a3a197cf21bbc0768269b6bba
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-07-19 11:38:18 +1000

    finished Sets

--- Sets.v
+++ Sets.v
@@ -99,8 +99,282 @@ Section U_SETS.
      apply In_right. trivial.
    Qed.
 
+ 
    Lemma Not_union :
      forall (E1 E2 : U_set) (x : U), ~E1 x -> ~E2 x -> ~ (Union E1 E2 x).
    Proof.
+     unfold not. intros. inversion H1. apply H. trivial.
+     apply H0; trivial.
+   Qed.
+
+  
+
+   Lemma Union_dec :
+     forall (e1 e2 : U_set) (x : U),
+       {e1 x} + {~ e1 x} -> {e2 x} + {~ e2 x} -> Union e1 e2 x -> {e1 x} + {e2 x}.
+   Proof.
+     intros; case H.
+     left; trivial.
+
+     intros; case H0; intros.
+     right; trivial.
+
+     absurd (Union e1 e2 x). apply Not_union; trivial.
+     trivial.
+
+   Qed.
+
+   Lemma Included_union : forall E F : U_set, Included E (Union E F).
+   Proof.
+     unfold Included; intros.
+     apply In_left; trivial.
+   Qed.
+
+   Lemma Union_absorb :
+     forall E F : U_set, Included E F -> Union E F = F.
+   Proof.
+     intros; apply U_set_eq; split; intros.
+     inversion H0; auto.
+
+     apply In_right; trivial.
+
+   Qed.
 
   End UNION.
+
+  Section INTESECTION.
+
+    Inductive Inter (E F : U_set) : U_set :=
+      In_inter: forall x : U, E x -> F x -> Inter E F x.
+
+    Lemma Inter_eq :
+      forall E F E' F' : U_set, E = E' -> F = F' -> Inter E F = Inter E' F'.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      rewrite <- H; rewrite <- H0; trivial.
+      rewrite H; rewrite H0; trivial.
+    Qed.
+
+    Lemma  Inter_neutral:
+      forall e : U_set, Inter Full e = e.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; trivial.
+      apply In_inter.
+      apply In_full; trivial.
+      trivial.
+    Qed.
+
+    Lemma Inter_empty :
+      forall e : U_set, Inter e Empty = Empty.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; trivial.
+      inversion H.
+    Qed.
+
+    Lemma Inter_commute :
+      forall e1 e2 : U_set, Inter e1 e2 = Inter e2 e1.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; constructor; trivial.
+      inversion H; constructor; trivial.
+    Qed.
+
+    Lemma Inter_assoc: forall e1 e2 e3 : U_set,
+        Inter e1 (Inter e2 e3) = Inter (Inter e1 e2) e3.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; inversion H1; apply In_inter. apply In_inter; trivial.
+      trivial.
+      inversion H; inversion H0; apply In_inter; trivial.
+      apply In_inter; trivial.
+    Qed.
+
+    Lemma Not_inter : forall (E1 E2 : U_set) (x : U),
+        ~ E1 x -> ~ Inter E1 E2 x.
+    Proof.
+      unfold not; intros.
+      inversion H0; apply H; trivial.
+    Qed.
+
+    Lemma Included_inter : forall E F : U_set,
+        Included (Inter E F) E.
+    Proof.
+      unfold Included; intros; inversion H; trivial.
+    Qed.
+
+    Lemma Inter_absorb : forall E F : U_set, Included E F -> Inter E F = E. 
+    Proof.
+      intros. apply U_set_eq; split; intros.
+      inversion H0; trivial.
+      apply In_inter; auto.
+    Qed.
+    
+  End INTESECTION.
+
+  Section DIFFERENCE.
+
+    Inductive Differ (E F : U_set) : U_set :=
+      In_differ : forall x : U, E x -> ~ F x -> Differ E F x.
+
+    Lemma Differ_E_F :
+      forall E : U_set, Differ E E = Empty.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; congruence.
+      inversion H.
+    Qed.
+
+    Lemma Differ_empty : forall E : U_set, Differ E Empty = E.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; trivial.
+      apply In_differ; trivial.
+      unfold not; intros; inversion H0.
+    Qed.
+
+    Lemma Union_differ_inter :
+      forall E F : U_set,
+        (forall x : U, {F x} + {~F x}) -> Union (Differ E F) (Inter E F) = E.
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H0.
+      inversion H1; trivial.
+      inversion H1; trivial.
+
+      case (H x); intros.
+      apply In_right; apply In_inter; trivial.
+      
+      apply In_left; apply In_differ; trivial.
+    Qed.
+    
+  End DIFFERENCE.
+
+  Section MIXED_PROPERTIES.
+
+    Lemma Distributivity_inter_union :
+      forall E F G : U_set, Inter E (Union F G) = Union (Inter E F) (Inter E G).
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H; inversion H1.
+      apply In_left; apply In_inter; auto.
+      apply In_right; apply In_inter; auto.
+
+      inversion H; inversion H0.
+      apply In_inter; auto.
+      apply In_left; auto.
+
+      apply In_inter; auto.
+      apply In_right; auto.
+    Qed.
+
+    Lemma Distributivity_union_inter :
+      forall E F G : U_set, Union E (Inter F G) = Inter (Union E F) (Union E G).
+    Proof.
+      intros; apply U_set_eq; split; intros.
+      inversion H.
+      apply In_inter; apply In_left; auto.
+      inversion H0; apply In_inter; apply In_right; auto.
+
+      inversion H.
+      inversion H0.
+
+      apply In_left; auto.
+
+      inversion H1.
+      apply In_left; trivial.
+
+      apply In_right; apply In_inter; trivial.
+    Qed.
+
+    Lemma Union_inversion :
+      forall E F G : U_set,
+        Inter E F = Empty -> Inter E G = Empty -> Union E F = Union E G -> F = G.
+    Proof.
+        intros; apply U_set_eq; split; intros.
+        generalize (In_right E F x H2).
+        rewrite H1; intros.
+        inversion H3.
+        absurd (Inter E F x).
+        rewrite H; tauto.
+
+        apply In_inter; auto.
+
+        trivial.
+
+        generalize (In_right E G x H2).
+        rewrite <- H1; intros.
+        inversion H3.
+        absurd (Inter E G x).
+        rewrite H0; tauto.
+
+        apply In_inter; auto.
+
+        trivial.
+    Qed.
+
+    Lemma Union_inversion2 :
+      forall E F G H : U_set,
+        Inter E F = Empty ->
+        Inter E G = Empty ->
+        Inter G H = Empty -> Union E F = Union G H -> F = Union G (Inter F H).
+    Proof.
+        intros; apply U_set_eq; split; intros.
+        generalize (In_right E F x H4).
+        rewrite H3; intros.
+        inversion H5.
+        apply In_left; trivial.
+
+        apply In_right; apply In_inter; trivial.
+
+        inversion H4.
+        generalize (In_left G H x H5).
+        rewrite <- H3; intros.
+        inversion H7.
+        absurd (Inter E G x).
+        rewrite H1; tauto.
+
+        apply In_inter; trivial.
+
+        trivial.
+
+        inversion H5; trivial.
+    Qed.
+
+    Lemma Single_disjoint :
+      forall (x : U) (E : U_set), ~ E x -> Inter (Single x) E = Empty.
+    Proof.
+        intros; apply U_set_eq; split; intros.
+        inversion H0.
+        inversion H1; absurd (E x).
+        trivial.
+
+        rewrite H4; trivial.
+
+        inversion H0.
+    Qed.
+
+    Lemma Single_single_disjoint :
+      forall x y : U, x <> y -> Inter (Single x) (Single y) = Empty.
+    Proof.
+        intros; apply Single_disjoint.
+        red; intros H0; elim H; inversion H0; trivial.
+    Qed.
+
+    Lemma Union_single_single :
+      forall (e : U_set) (x y : U),
+        x <> y ->
+        ~ e y -> Union (Single x) (Single y) = Union (Single y) e -> e = Single x.
+    Proof.
+        intros; symmetry ; apply (Union_inversion (Single y)).
+        apply Single_single_disjoint; auto.
+
+        apply Single_disjoint; trivial.
+
+        rewrite Union_commut; trivial.
+    Qed.
+
+  End MIXED_PROPERTIES.
+
+End U_SETS.
